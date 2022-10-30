fd2ae1af55387b11cf2587f664e6788f375f3e05
valentinesamuel <valentinesamuel2580@gmail.com>
2022-10-30 09:04:03 +0100

    fix: pycodestyle compliant

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-
 import models
 
 """The Base Model contains instances and methods for other classes"""
 
+
 class BaseModel:
-    """BaseModel class definition"""    
+    """BaseModel class definition"""
     pass
