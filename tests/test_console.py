9a44343122dfa91d512bebf3c89ed65d5d4f3e56
valentinesamuel <valentinesamuel2580@gmail.com>
2022-10-31 07:16:51 +0100

    fix: console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
 """Test Console"""
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+import sys
+from io import StringIO
 
 
-class TestConsole(unittest.TestCase):
+class TestConsolePrompt(unittest.TestCase):
     """
-    This test checks if all required classes
-    are created correctly.
+    This class provides all possible test cases regarding prompt
+    response of class HBNBCommand.
     """
-    def test_class(self):
-        """
-        This test checks if all required classes
-        are present
-        """
-        city1 = City()
-        amenity1 = Amenity()
-        state1 = State()
-        rev1 = Review()
-        place1 = Place()
-        self.assertEqual(city1.__class__.__name__, "City")
-        self.assertEqual(amenity1.__class__.__name__, "Amenity")
-        self.assertEqual(state1.__class__.__name__, "State")
-        self.assertEqual(rev1.__class__.__name__, "Review")
-        self.assertEqual(place1.__class__.__name__, "Place")
+    def test_prompt_output(self):
+        self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
 
-    def test_father(self):
-        """
-        This test checks if all required classes
-        inherit correcly from BaseModel
-        """
-        city1 = City()
-        amenity1 = Amenity()
-        state1 = State()
-        rev1 = Review()
-        place1 = Place()
-        self.assertTrue(issubclass(city1.__class__, BaseModel))
-        self.assertTrue(issubclass(amenity1.__class__, BaseModel))
-        self.assertTrue(issubclass(state1.__class__, BaseModel))
-        self.assertTrue(issubclass(rev1.__class__, BaseModel))
-        self.assertTrue(issubclass(place1.__class__, BaseModel))
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
